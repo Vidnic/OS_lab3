c83fc300382829e6068ca51535b93b4848839a07
Vidnic <pavelviduk54@gmail.com>
2026-04-21 17:09:38 +0300

    Delete build directory

--- build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/USER/source/repos/os_lab3/build/_deps/googletest-src
-# Build directory: C:/Users/USER/source/repos/os_lab3/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/USER/source/repos/os_lab3/build/_deps/googletest-src/googletest
-# Build directory: C:/Users/USER/source/repos/os_lab3/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
